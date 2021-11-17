7f572605179a83257042bf02db8e1f66ffa5dfce
michmitch <62762271+michmitch@users.noreply.github.com>
2021-11-17 11:49:47 +0700

    Update test_movies.py

--- test_movies.py
+++ test_movies.py
@@ -1,15 +1,19 @@
 import unittest
-
-from werkzeug.exceptions import HTTPException
 import movies
 import models
 
 class TestMovies(unittest.TestCase):
     def setUp(self):
-        # Load test data
+        """
+        Inisialisasi
+        """
         self.app = models.Movies
 
     def test_movie(self):
+        """
+        Testing ambil 1 data movies apakah id dan title sesuai
+        serta status code nya 200 ketika berhasil
+        """
         movie, status = movies.read_one(7111, 48401)
         self.assertEqual(movie['title'], 'Good')
         self.assertEqual(status, 200)
