3c3e55bd107a92d3446916ead779d82b9036ac18
michmitch <62762271+michmitch@users.noreply.github.com>
2021-11-17 11:49:26 +0700

    Update test_directors.py

--- test_directors.py
+++ test_directors.py
@@ -4,14 +4,22 @@ import models
 
 class TestDirectors(unittest.TestCase):
     def setUp(self):
-        # Load test data
+        """
+        Inisialisasi
+        """
         self.app = models.Directors
 
     def test_director_read_one(self):
+        """
+        Testing ambil 1 director apakah id dan namanya sesuai
+        """
         director_check = directors.read_one(7111)
         self.assertEqual(director_check['name'], 'Director Gusti')
     
     def test_update_director(self):
+        """
+        Testing update apakah status codenya ketika berhasil akan 200
+        """
         updated_director = {
             'name': 'Test Edit Director',
             'gender': 0,
